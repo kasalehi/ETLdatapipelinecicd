3e4ecac32d33db5a5af71c5f818f97cb6edf0430
kasalehi <ksalehi56@yahoo.com>
2025-01-10 15:32:21 +1300

    firstcommit

--- app.py
+++ app.py
@@ -1,2 +1,30 @@
+import requests
 import pandas as pd 
-print("hello")
+import datetime
+
+logfile='logfile.txt'
+def log_msg(masg):
+     with open(logfile, 'a') as f:
+        f.write(f'{datetime.datetime.now()} - {masg} \n')
+    
+#lets read csv file form api
+try: 
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    data = response.json()
+
+    # Convert to DataFrame
+    df = pd.DataFrame(data)
+
+    # Transform
+    df['title_length'] = df['title'].apply(len)
+    df['body_length'] = df['body'].apply(len)
+    etl_timestamp = datetime.datetime.now()
+    df['etl_timestamp'] = etl_timestamp
+#lets save tocsv 
+     # Load
+    df.to_csv('transformed_data.csv', index=False)
+    log_msg(f'ETL job completed at {etl_timestamp} and data saved to transformed_data.csv')
+
+except Exception as e:
+     log_msg(f"Error {e} at {datetime.datetime.now()}")
+    
